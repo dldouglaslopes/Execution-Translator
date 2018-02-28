80e17f2e41f970bff7af7ca9a9e69c66fd460478
dldouglaslopes <dldouglaslopes@gmail.com>
2018-02-27 23:47:36 -0300

    new classes added

--- src/main/java/Translator.java
+++ src/main/java/Translator.java
@@ -1,9 +1,11 @@
-
-
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.json.JSONObject;
 
 public class Translator {
 	public void createXMI(JSONObject json){
+		String type = json.getString("type");
 		
+		ResourceSet resourceSet = new ResourceSetImpl();
 	}
 }
